963012a264971c8f0e68a0a9494915eead8ee33f
ryou <ryou@ryouui-MacBookAir.local>
2023-05-03 02:24:35 +0900

    git action to auto build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withExportImages = require("next-export-optimize-images");
 const nextConfig = withExportImages({
   reactStrictMode: false,
   assetPrefix: ".", // for relative path
+  trailingSlash: true, // for static export
 });
 
 module.exports = nextConfig;
